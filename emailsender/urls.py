fbeec7ec02965e270671f4a826d30458f1b2f60b
Uglypr1nces <shpatavdiu4@gmail.com>
2024-03-05 07:38:55 +0100

    fixed the issues with the starting of the project

--- emailsender/urls.py
+++ emailsender/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from emailsending import views
-from django.conf.urls import url
+from django.urls import include
 
 urlpatterns = [
     path('', views.index, name='index'),
