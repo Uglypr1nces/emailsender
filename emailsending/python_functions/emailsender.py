c109d47c678b1dd8a5282a6a519c2037df6d3e1e
Uglypr1nces <shpatavdiu4@gmail.com>
2024-03-05 10:07:00 +0100

    fixed errors, changed design, added a sender email

--- emailsending/python_functions/emailsender.py
+++ emailsending/python_functions/emailsender.py
@@ -1,2 +1,12 @@
-def send(self_email,email, subject, message):
-    print(f'Email sent from {self_email} to {email} with subject {subject} and message {message}')
+import smtplib
+
+def send(self_email,reciever_email, subject, message):
+    server = smtplib.SMTP('smtp.gmail.com', 587) #port 465 or 587
+    server.starttls() 
+
+    server.login(self_email, "cffs ehnp wlca sxwd")
+
+    server.sendmail(self_email, reciever_email, f"Subject: {subject}\n\n{message}")
+
+    print("EMAIL HAS BEEN SENT SUCCESSFULLY")
+   
--- emailsending/views.py
+++ emailsending/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .python_functions.emailsender import *
 from django.views.decorators.csrf import csrf_exempt
+import json
 # Create your views here.
 
 def index(request):
@@ -9,10 +10,14 @@ def index(request):
 
 def send_email(request):
     if request.method == 'POST':
-        self_email = request.POST.get('self_email')
-        email = request.POST.get('reciever_email')
-        subject = request.POST.get('subject')
-        message = request.POST.get('message')
+        data = json.loads(request.body.decode('utf-8'))
+        self_email = "unattractivequeen@gmail.com"
+        email = data.get('reciever_email')
+        subject = data.get('subject')
+        message = data.get('message')
         print(f'Email sent from {self_email} to {email} with subject {subject} and message {message}')
-        send(self_email,email, subject, message)
+        try:
+            send(self_email,email, subject, message)
+        except:
+            return HttpResponse('Email Not Sent, Please check your credentials and try again.')
         return HttpResponse('Email Sent')
